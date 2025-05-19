890671b1914c9dd7931465ed55a601022977e46e
Ahmet <99803359+611527@users.noreply.github.com>
2025-05-19 14:34:15 +0200

    hotfix

--- src/main/java/workshop/ClusteringDemo.java
+++ src/main/java/workshop/ClusteringDemo.java
@@ -1,18 +1,36 @@
 package workshop;
-
+ 
 import weka.clusterers.SimpleKMeans;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
-
+ 
 public class ClusteringDemo {
     public static void run() throws Exception {
-        Instances data = new DataSource("datasets/iris.arff").getDataSet();
-
-        SimpleKMeans kmeans = new SimpleKMeans();
-        kmeans.setNumClusters(3);  // TODO: Make number of clusters configurable
-        kmeans.buildClusterer(data);
-
-        System.out.println("=== Clustering Results ===");
-        System.out.println(kmeans);
+        System.out.println("Running ClusteringDemo...");
+ 
+        // Load dataset
+        DataSource source = new DataSource("datasets/clustering_data.arff");
+        Instances data = source.getDataSet();
+ 
+        // Initialize clusterer
+        SimpleKMeans model = new SimpleKMeans();
+        model.setNumClusters(3);
+        model.setSeed(1);
+        model.setPreserveInstancesOrder(true);
+ 
+        // Build the model
+        model.buildClusterer(data);
+ 
+        // Output
+        System.out.println("\nClustering Results\n======\n");
+        System.out.println("Number of clusters: " + model.getNumClusters());
+        System.out.println("Cluster centroids:\n" + model);
+ 
+        // Assignments
+        int[] assignments = model.getAssignments();
+        System.out.println("Instance Cluster Assignments:");
+        for (int i = 0; i < assignments.length; i++) {
+            System.out.printf("Instance %d -> Cluster %d\n", i, assignments[i]);
+        }
     }
 }
